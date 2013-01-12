2b7a3e4dd8db07dd0f1995f3ed11fc11337f9103
Matthew Gould <matt@curebit.com>
2013-01-12 17:32:03 -0500

    Initial Commit:
    This is the initial commit for Chapter 3 of the rails tutorial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigApp::Application.config.secret_token = '08d46df4a808edadd1e33b545820671b008b8c49ea2180794e73755312e85e637255c253dee666beffc36b9d5c579beb08fda25c863d4705ce3bc75bf7857234'
