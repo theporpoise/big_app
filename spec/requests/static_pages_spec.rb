760cb6f867a0ecaa1373ee4654f2f99ba31a4a55
Matthew Gould <matt@curebit.com>
2013-01-13 08:13:42 -0500

    Added a contact us page
    Also added tests for this page, configured the routes, put in some string interpolation in the static_pages_spec.rb test, and of course created the contact.html.erb page that used the template application.html.erb with the substitution for the title.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 #The tests for all the static pages
 describe "StaticPages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Big App"}
   
   #Home page test
   describe "Home Page" do
@@ -15,7 +17,7 @@ describe "StaticPages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Big App | Home")
+        :text => "#{base_title} | Home")
     end
 
   end
@@ -31,7 +33,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Big App | Help")
+                        :text => "#{base_title} | Help")
     end
 
   end
@@ -47,9 +49,22 @@ describe "StaticPages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Big App | About Us")
+        :text => "#{base_title} | About Us")
     end
 
+  end
+
+  describe "Contact Page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+        :text => "#{base_title} | Contact Us")
+    end
 
   end
 
