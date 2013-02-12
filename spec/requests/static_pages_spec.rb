9718f9c9544774b461fe6d665777eb90de02fb89
Matthew Gould <matt@curebit.com>
2013-02-12 10:36:41 -0500

    Removed the standard home page, replaced with mine

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,18 +10,18 @@ describe "StaticPages" do
     
     it "should have the h1 'Big App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Big App')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
         :text => "#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => '| Home')
     end
 
@@ -31,12 +31,12 @@ describe "StaticPages" do
   describe "Help page" do
     
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
 	   end 
   
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "#{base_title} | Help")
     end
@@ -47,12 +47,12 @@ describe "StaticPages" do
   #About Page Tests
   describe "About page" do
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
         :text => "#{base_title} | About Us")
     end
@@ -63,12 +63,12 @@ describe "StaticPages" do
 
   describe "Contact Page" do
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
         :text => "#{base_title} | Contact Us")
     end
