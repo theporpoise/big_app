730b6470b580c890b63518c5e6343c4ad5ec2e8b
Matthew Gould <matt@curebit.com>
2013-02-12 14:47:13 -0500

    prettier static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,76 +3,41 @@ require 'spec_helper'
 #The tests for all the static pages
 describe "StaticPages" do
 
-  let(:base_title) {"Ruby on Rails Tutorial Big App"}
-  
+  #let(:base_title) {"Ruby on Rails Tutorial Big App"}
+  subject { page }
+
   #Home page test
   describe "Home Page" do
+    before { visit root_path }
     
-    it "should have the h1 'Big App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit root_path
-      page.should have_selector('h1', :text => 'Big App')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',
-        :text => "#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', :text => '| Home')
-    end
-
+    it { should have_selector 'h1', text: 'Big App' }
+    it { should have_selector 'title', text: full_title('') }
+    it { should_not have_selector 'title', text: '| Home' }  
   end
 
   #Help Page Tests
   describe "Help page" do
+    before { visit help_path }
     
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-	   end 
-  
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                        :text => "#{base_title} | Help")
-    end
-
+    it { should have_selector 'h1', text: 'Help' }
+	  it { should have_selector 'title', text: full_title('') }
   end
-
 
   #About Page Tests
   describe "About page" do
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_selector('title',
-        :text => "#{base_title} | About Us")
-    end
-
+    before { visit about_path }
+    
+    it { should have_selector 'h1', text: 'About Us' }
+    it { should have_selector 'title', text: full_title('') }
   end
 
   #Contact Page Tests
 
   describe "Contact Page" do
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-        :text => "#{base_title} | Contact Us")
-    end
-
+    it { should have_selector 'h1', text: 'Contact' }
+    it { should have_selector 'title', text: full_title('') }
   end
 
 end
