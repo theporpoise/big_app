d39310304b0e30f941a28cd1ae679e7454ec06c7
Matthew Gould <matt@curebit.com>
2013-02-16 13:32:16 -0500

    testing to make sure that must be signed in to visit the user edit page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,7 +15,10 @@ describe "User Pages" do
 
 	describe "edit" do
 		let(:user) {FactoryGirl.create(:user) }
-		before { visit edit_user_path(user) }
+		before do
+			valid_signin user
+			visit edit_user_path(user)
+		end
 
 		describe "page" do
 			it { should have_selector('h1', text: "Update your profile") }
