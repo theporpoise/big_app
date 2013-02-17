3ea1e7223c2cd4a7bcfdea1c12a11191bb9ed561
Matthew Gould <matt@curebit.com>
2013-02-16 20:32:42 -0500

    added faker to the gemfile to create fake users, created a lib tasks rake file to create fake users, addeed some css to the index for the users page, changed teh users controller to point to the index page and assign the @users variable to an array, created a users index page that iterated through users and printed out data, created the associated tests, updated teh header to include a users tab to see all the users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,6 +26,7 @@ describe "Authentication" do
 			let(:user) {FactoryGirl.create(:user) }
 			before {valid_signin(user) }
 
+			it { should have_link('Users', href: users_path) }
 			it { should have_selector('title', text: user.name) }
 			it { should have_link('Profile', href: user_path(user)) }
 			it { should have_link('Settings', href: edit_user_path(user)) }
@@ -55,6 +56,12 @@ describe "Authentication" do
 					before { put user_path(user) }
 					specify { response.should redirect_to(signin_path) }
 				end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_selector('title', text: 'Sign in') }
+				end
+
 			end
 
 			describe "when attempting to visit a protected page" do
