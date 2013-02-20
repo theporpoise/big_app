1241782d9b2a0f7d955742b31b1bb1c0cb44199e
Matthew Gould <matt@curebit.com>
2013-02-20 09:15:19 -0500

    had to comment out the auto generated factories file because it created a duplicate microposts factory.  not sure why this came up in the rails tutorial.  prolly my gem file doesn't match up exactly and factory girl had some sort of gem update.

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -1,8 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :micropost do
-    content "MyString"
-    user_id 1
-  end
-end
+#FactoryGirl.define do
+#  factory :micropost do
+#    content "MyString"
+#    user_id 1
+#  end
+#end
