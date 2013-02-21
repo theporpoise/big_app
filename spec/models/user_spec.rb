872ca96fbc4a2d7cff22e021ff7bdc13f23064e8
Matthew Gould <matt@curebit.com>
2013-02-21 16:28:22 -0500

    ok, got it all set up and working.  I am lost in the woods here.  But that's ok.  Neat way to associate the following and followers relationship through the relationship table, just using reverse lookings, i.e. passing in the other paramater backwards to get it to work.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,7 +38,7 @@ describe User do
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
   it { should respond_to(:reverse_relationships) }
-  it { shoudl respond_to(:followers) }
+  it { should respond_to(:followers) }
 
   it {should be_valid }
   it {should_not be_admin }
